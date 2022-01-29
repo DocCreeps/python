51b2eb6d5ac62e8228d765b8566ae13d23db0cfd
DocCreeps <dorian1998dino@hotmal.com>
2022-01-29 14:50:20 +0100

    init

---
+++ TTS/TTS.py
@@ -0,0 +1,38 @@
+import pyttsx3
+
+# initialize Text-to-speech engine
+engine = pyttsx3.init()
+
+# convert this text to speech
+text = input("Le text a lire : ")
+
+
+engine.say(text)
+# play the speech
+engine.runAndWait()
+
+# get details of speaking rate
+rate = engine.getProperty("rate")
+print(rate)
+
+# setting new voice rate (faster)
+engine.setProperty("rate", 300)
+engine.say(text)
+engine.runAndWait()
+
+# slower
+engine.setProperty("rate", 100)
+engine.say(text)
+engine.runAndWait()
+
+# get details of all voices available
+voices = engine.getProperty("voices")
+print(voices)
+# set another voice
+engine.setProperty("voice", voices[2].id)
+engine.say(text)
+engine.runAndWait()
+
+# saving speech audio into a file
+engine.save_to_file(text, "python.mp3")
+engine.runAndWait()
